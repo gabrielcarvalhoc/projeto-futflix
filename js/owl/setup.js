c77354d351c99599fadd8be1404d4e5cc72bfd3e
gabrielcarvalhoc <gabrielcarvalho@poli.ufrj.br>
2021-05-12 18:12:47 -0300

    finalizar projeto futflix

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:6,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        500:{
+            items:4
+        },
+        1100:{
+            items:6
+        }
+    }
+})
